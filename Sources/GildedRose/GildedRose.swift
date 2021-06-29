e9e71097d490187e432d972311b69a6224e47d37
Dennis Litjens <>
2021-06-29 22:34:43 +0200

    Refactor updateQuality to seperate methods

--- Sources/GildedRose/GildedRose.swift
+++ Sources/GildedRose/GildedRose.swift
@@ -1,34 +1,48 @@
 public class GildedRose {
     var items:[Item]
     
+    let MIN_QUALITY = 0
+    let MAX_QUALITY = 50
+    
     public init(items:[Item]) {
         self.items = items
     }
     
     public func updateQuality() {
         for item in items {
-            switch item.isQualityOperation() {
-            case .doesNotChange:
-                return
-            case .dropToZero:
-                item.quality = 0
-            case .degrades(let degradeValue):
-                if item.quality - degradeValue >= 0 {
-                    item.quality -= degradeValue
-                } else {
-                    item.quality = 0
-                }
-            case .increases(let increaseValue):
-                if item.quality + increaseValue <= 50 {
-                    item.quality += increaseValue
-                } else {
-                    item.quality = 50
-                }
-            }
-            
-            if item.sellIn >= 0 {
-                item.sellIn -= 1
-            }
+            item.quality = self.calculateQualityFor(
+                qualityOperation: item.isQualityOperation(),
+                from: item.quality
+            )
+            item.sellIn = self.calculateSellIn(forItem: item)
         }
+    }
+    
+    private func calculateQualityFor(qualityOperation: QualityOperation, from quality: Int) -> Int {
+        switch qualityOperation {
+        case .doesNotChange:
+            return quality
+        case .dropToZero:
+            return 0
+        case .degrades(let degradeValue):
+            return self.degrade(quality, withDegradeValue: degradeValue)
+        case .increases(let increaseValue):
+            return self.increase(quality, withIncreaseValue: increaseValue)
+        }
+    }
+    
+    private func calculateSellIn(forItem item: Item) -> Int {
+        if item.sellIn >= 0 && item.isTypeOff() != .sulfurasHandOfRagnaros {
+            return item.sellIn - 1
+        }
+        return item.sellIn
+    }
+    
+    private func degrade(_ quality: Int, withDegradeValue degradeValue: Int) -> Int {
+        return quality - degradeValue >= MIN_QUALITY ? quality - degradeValue : MIN_QUALITY
+    }
+    
+    private func increase(_ quality: Int, withIncreaseValue increaseValue: Int) -> Int {
+        return quality + increaseValue <= MAX_QUALITY ? quality + increaseValue : MAX_QUALITY
     }
 }
